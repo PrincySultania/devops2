9e1ef0330c845ef35916e453236bf62fbd381c9a
Princy Sultania <princyjune66@gmail.com>
2020-05-26 13:35:51 +0530

    this is 18 test

--- keras_mnistcode.py
+++ keras_mnistcode.py
@@ -1,31 +1,17 @@
 print("hello princy")
 print("it is  working fine")
-
 print("finally done")
-
-keras[D[D[D[D[Dimport keras
-
+import keras
 print("it should run")
-
 print("it is running")
-
-print(...)
-
 print('!')
-
 print("its done")
-
 print("hi")
-
 print("done")
-
 print("what to do")
-
 print("hopefully")
-
 print("ho jao na plsss")
-
 print("new")
 print("i think its done")
-
 print("Its almost there")
+print("finally its done")
